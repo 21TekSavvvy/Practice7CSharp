9710a929d7001a73462d79d0eb4a06d8100af191
21TekSavvvy <jareddarochajj@gmail.com>
2022-02-10 16:30:09 -0800

    Final

---
+++ Practice7/Practice7/Program.cs
@@ -0,0 +1,62 @@
+﻿class loops
+{
+    static void Main()
+    {
+        for(int i = 1; i <11;i++)
+        {
+            if (i == 5 || i==6)
+            {
+                continue;
+            }
+            System.Console.Write(i+ " ");
+        }
+        System.Console.WriteLine();
+        for (int i = 1; i < 11; i++)
+        {
+            if (i == 5 || i == 6)
+            {
+                continue;
+            }
+            System.Console.Write(i + " ");
+        }
+        System.Console.WriteLine();
+        for (int i = 1; i < 11; i++)
+        {
+            if (i == 5 || i == 6)
+            {
+                continue;
+            }
+            System.Console.Write(i + " ");
+        }
+        System.Console.WriteLine();
+        for (int i = 10; i >0; i--)
+        {
+            System.Console.Write(i + " ");
+        }
+        System.Console.WriteLine();
+        for (int i = 10; i > 0; i--)
+        {
+            System.Console.Write(i + " ");
+        }
+        System.Console.WriteLine();
+        for (int i = 10; i > 0; i--)
+        { 
+            if (i == 2)
+            {
+                break;
+            }
+            System.Console.Write(i + " ");
+        }
+        System.Console.WriteLine();
+        for (int i = 10; i > 0; i--)
+        {
+            System.Console.Write(i + " ");
+        }
+        System.Console.WriteLine();
+        for (int i = 1; i < 11; i++)
+        {
+            System.Console.Write(i + " ");
+        }
+        System.Console.ReadKey();
+    }
+}
